714ed52c05b015f1f2879e4b75f0f345b91e9c53
brunobelloni <bruno.belloni@hotmail.com>
2019-03-28 19:01:39 -0300

    thread stuffs

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,12 @@ using namespace std;
 
 int HOW_MANY_READERS = 4;
 int HOW_MANY_WRITERS = 4;
+
 double READERS_INTENSITY = 0.2;
 double WRITERS_INTENSITY = 0.5;
+
 int MAX_PROCESSING_TIME = 10; // seconds
+int MIN_PROCESSING_TIME = 2; // seconds
 
 enum TYPE {
     WRITER,
@@ -15,67 +18,58 @@ enum TYPE {
 };
 
 class File {
-  private:
+private:
     string data;
     int activeReaders;
     int activeWriters;
-  public:
-  
+public:
+
     File(string data) {
         this->data = data;
         this->activeReaders = 0;
         this->activeWriters = 0;
     }
-    
+
     bool canRead() {
         return this->activeWriters <= 0 ? true : false;
     }
-        
+
     string getData() {
         return this->data;
     }
-    
+
     void setData(string data) {
         this->data = data;
     }
-    
+
     int getActiveReaders() {
         return this->activeReaders;
     }
-    
+
     void setActiveReaders(int activeReaders) {
         this->activeReaders = activeReaders;
     }
-    
+
     int getActiveWriters() {
         return this->activeWriters;
     }
-    
+
     void setActiveWriters(int activeWriters) {
         this->activeWriters = activeWriters;
     }
 };
 
 void foo() {
-  // do stuff...
-}
-
-void bar(int x) {
-  // do stuff...
+    cout << "Chamou!" << endl;
 }
 
 int main() {
 
-  thread first(foo);     m// spawn new thread that calls foo()
-  thread second(bar, 0);  // spawn new thread that calls bar(0)
+    std::thread first(foo);
 
-  std::cout << "main, foo and bar now execute concurrently...\n";
+    first.join();
 
-  // synchronize threads:
-  first.join();                // pauses until first finishes
-  second.join();               // pauses until second finishes
+    cout << "foo and bar completed.\n";
 
-  cout << "foo and bar completed.\n";
-    
-  return 0;
+    return 0;
 }
