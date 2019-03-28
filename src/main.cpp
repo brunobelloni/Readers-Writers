4cb5b21a1eda7b6f301d7ca6c9542897f8de074c
Bruno Belloni <bruno.belloni@hotmail.com>
2019-03-28 13:58:21 -0300

    Readers and Writers class

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,33 @@
 using namespace std;
 
 enum TYPE {
-    WRITE,
-    READ,
-    DESTROY,
-    UPDATE,
+    WRITER,
+    READER,
+};
+
+class File {
+  private:
+    string data;
+    int activeReaders;
+    int activeWriters;
+  public:
+  
+    File(string data) {
+        this->data = data;
+        this->activeReaders = 0;
+        this->activeWriters = 0;
+    }
+    
+    string getData() {
+        return this->data;
+    }
 };
 
 int main() {
 
-
-
-
+    File *file = new File("Hello World");
+    
+    cout << file->getData() << endl;
+    
     return 0;
 }
